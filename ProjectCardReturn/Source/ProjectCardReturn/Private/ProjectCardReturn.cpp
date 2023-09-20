2622bc10a955f0dabb537d3ae7c4e580b211066d
mintyway0112 <mintyway0112@gmail.com>
2023-09-20 20:06:08 +0900

    Init

---
+++ ProjectCardReturn/Source/ProjectCardReturn/Private/ProjectCardReturn.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ProjectCardReturn.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ProjectCardReturn, "ProjectCardReturn" );
