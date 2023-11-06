48f245a5016663c41f31c073c53991055269f9cf
PM 하예찬 <yechandubai@gmail.com>
2023-11-06 16:45:52 +0900

    힐 아이템 추가

---
+++ ProjectCardReturn/Source/ProjectCardReturn/Public/Entities/Item/PCRHealItem.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Entities/Item/PCRBaseItem.h"
+#include "PCRHealItem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTCARDRETURN_API APCRHealItem : public APCRBaseItem
+{
+	GENERATED_BODY()
+	
+};
