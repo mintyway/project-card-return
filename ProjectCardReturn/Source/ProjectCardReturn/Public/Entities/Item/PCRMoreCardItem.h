0352731feb29028d39e49a24f1fb666d8d1616a7
PM 하예찬 <yechandubai@gmail.com>
2023-11-06 16:44:26 +0900

    카드 증감 아이템 추가

---
+++ ProjectCardReturn/Source/ProjectCardReturn/Public/Entities/Item/PCRMoreCardItem.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Entities/Item/PCRBaseItem.h"
+#include "PCRMoreCardItem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTCARDRETURN_API APCRMoreCardItem : public APCRBaseItem
+{
+	GENERATED_BODY()
+	
+};
