c15cad0c9c0aad6cad1c3d7306e01bb4362827bc
PM 하예찬 <yechandubai@gmail.com>
2023-11-06 16:55:54 +0900

    사거리 증가 아이템 추가

---
+++ ProjectCardReturn/Source/ProjectCardReturn/Public/Entities/Item/PCRLongerRangeItem.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "ProjectCardReturn.h"
+#include "Entities/Item/PCRBaseItem.h"
+#include "PCRLongerRangeItem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTCARDRETURN_API APCRLongerRangeItem : public APCRBaseItem
+{
+	GENERATED_BODY()
+	
+};
