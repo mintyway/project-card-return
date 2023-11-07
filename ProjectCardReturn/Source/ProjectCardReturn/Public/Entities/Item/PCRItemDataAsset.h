67e72800c2582d9353f37fe5d0c47d60eaa737a2
PM 하예찬 <yechandubai@gmail.com>
2023-11-07 19:43:57 +0900

    데이터 에셋 추가

--- ProjectCardReturn/Source/ProjectCardReturn/Public/Entities/Item/PCRItemDataAsset.h
+++ ProjectCardReturn/Source/ProjectCardReturn/Public/Entities/Item/PCRItemDataAsset.h
@@ -6,6 +6,7 @@
 #include "Engine/DataAsset.h"
 #include "PCRItemDataAsset.generated.h"
 
+class UNiagaraComponent;
 /**
  * 
  */
@@ -13,5 +14,23 @@ UCLASS()
 class PROJECTCARDRETURN_API UPCRItemDataAsset : public UDataAsset
 {
 	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditDefaultsOnly, Category = "Item")
+	TObjectPtr<UNiagaraComponent> SpeedUpItemEffect;
 	
+	UPROPERTY(EditDefaultsOnly, Category = "Item")
+	TObjectPtr<UNiagaraComponent> MoreHpItemEffect;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Item")
+	TObjectPtr<UNiagaraComponent> ManyCardItemEffect;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Item")
+	TObjectPtr<UNiagaraComponent> HealItemEffect;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Item")
+	TObjectPtr<UNiagaraComponent> FastShootItemEffect;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Item")
+	TObjectPtr<UNiagaraComponent> LongRangeItemEffect;
 };
