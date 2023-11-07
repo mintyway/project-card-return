7f17661825421ce4419d50d4cdf3f5f4107a68ab
PM 하예찬 <yechandubai@gmail.com>
2023-11-07 16:19:47 +0900

    체력 업 아이템 추가

---
+++ ProjectCardReturn/Source/ProjectCardReturn/Public/Entities/Item/PCRMoreHpItem.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Entities/Item/PCRBaseItem.h"
+#include "PCRMoreHpItem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTCARDRETURN_API APCRMoreHpItem : public APCRBaseItem
+{
+	GENERATED_BODY()
+	
+};
