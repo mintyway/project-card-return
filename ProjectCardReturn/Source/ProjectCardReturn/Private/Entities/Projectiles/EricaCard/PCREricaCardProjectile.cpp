ab3a9b4596606c8575aedce5fa4250436b067b06
PM 하예찬 <yechandubai@gmail.com>
2023-11-07 10:05:16 +0900

    아이템 콜리젼 추가

--- ProjectCardReturn/Source/ProjectCardReturn/Private/Entities/Projectiles/EricaCard/PCREricaCardProjectile.cpp
+++ ProjectCardReturn/Source/ProjectCardReturn/Private/Entities/Projectiles/EricaCard/PCREricaCardProjectile.cpp
@@ -174,6 +174,7 @@ void APCREricaCardProjectile::EnableCollisionDetection()
 		BoxComponent->SetCollisionResponseToChannel(ECC_GameTraceChannel6, ECR_Block);
 		BoxComponent->SetCollisionResponseToChannel(ECC_GameTraceChannel8, ECR_Block);
 		BoxComponent->SetCollisionResponseToChannel(ECC_GameTraceChannel9, ECR_Overlap);
+		BoxComponent->SetCollisionResponseToChannel(ECC_GameTraceChannel10, ECR_Block);
 	}
 }
 
