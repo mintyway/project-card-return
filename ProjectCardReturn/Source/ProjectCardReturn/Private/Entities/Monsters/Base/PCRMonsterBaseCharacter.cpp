27b4b6981c7b63584042dd553a6c2bc9152dc415
PM 하예찬 <yechandubai@gmail.com>
2023-11-14 13:26:36 +0900

    확률 임시 조정 및 아이템 파괴 시간 조정

--- ProjectCardReturn/Source/ProjectCardReturn/Private/Entities/Monsters/Base/PCRMonsterBaseCharacter.cpp
+++ ProjectCardReturn/Source/ProjectCardReturn/Private/Entities/Monsters/Base/PCRMonsterBaseCharacter.cpp
@@ -235,7 +235,7 @@ void APCRMonsterBaseCharacter::HandleDead()
 	FTimerHandle SpawnItemTimeHandle;
 	FTimerDelegate SpawnItemTimeDelegate;
 	SpawnItemTimeDelegate.BindUObject(this, &APCRMonsterBaseCharacter::SpawnItem);
-	GetWorldTimerManager().SetTimer(SpawnItemTimeHandle, SpawnItemTimeDelegate, ParameterDataAsset->DeadAfterDestroyTime, false);
+	GetWorldTimerManager().SetTimer(SpawnItemTimeHandle, SpawnItemTimeDelegate, ParameterDataAsset->DeadAfterDestroyTime * 0.9f, false);
 }
 
 void APCRMonsterBaseCharacter::SpawnItem()
@@ -248,13 +248,21 @@ void APCRMonsterBaseCharacter::SpawnItem()
 
 UClass* APCRMonsterBaseCharacter::GetItemClass()
 {
-	float SpeedUpItemRate = 0.0f;
+	/*float SpeedUpItemRate = 0.0f;
 	float MoreHpItemRate = 0.3f;
 	float ManyCardItemRate = 0.2f;
 	float HealItemRate = 0.1f;
 	float FastShootItemRate = 0.0f;
 	float StrongAttackItemRate = 0.25f;
-	float LongerRangeItemRate = 0.15f;
+	float LongerRangeItemRate = 0.15f;*/
+
+	float SpeedUpItemRate = 0.0f;
+	float MoreHpItemRate = 0.5f;
+	float ManyCardItemRate = 0.0f;
+	float HealItemRate = 0.5f;
+	float FastShootItemRate = 0.0f;
+	float StrongAttackItemRate = 0.0f;
+	float LongerRangeItemRate = 0.0f;
 
 	const float RangeMax
 		= SpeedUpItemRate
