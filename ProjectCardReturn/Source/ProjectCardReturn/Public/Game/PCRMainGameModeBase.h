8611f06a2174ab88ab5db35e98aab8269dc4fce2
TD 이동우 <mintyway0112@gmail.com>
2023-11-21 16:11:46 +0900

    메인 UI 임포트

---
+++ ProjectCardReturn/Source/ProjectCardReturn/Public/Game/PCRMainGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "ProjectCardReturn.h"
+#include "GameFramework/GameModeBase.h"
+#include "PCRMainGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTCARDRETURN_API APCRMainGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	APCRMainGameModeBase();
+};
