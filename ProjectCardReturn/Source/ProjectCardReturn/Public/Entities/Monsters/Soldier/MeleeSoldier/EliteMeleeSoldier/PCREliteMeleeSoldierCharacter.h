cad643ff1d277308e88854f8b25e9bb82820fa48
PM 하예찬 <yechandubai@gmail.com>
2023-11-23 15:00:31 +0900

    엘리트 몬스터 클래스 생성
    엘리트 토끼 클래스 생성
    
    엘리트 근접 병사 클래스 생성
    
    엘리트 원거리 병사 클래스 생성

---
+++ ProjectCardReturn/Source/ProjectCardReturn/Public/Entities/Monsters/Soldier/MeleeSoldier/EliteMeleeSoldier/PCREliteMeleeSoldierCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "ProjectCardReturn.h"
+#include "Entities/Monsters/Soldier/MeleeSoldier/PCRMeleeSoldierCharacter.h"
+#include "PCREliteMeleeSoldierCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTCARDRETURN_API APCREliteMeleeSoldierCharacter : public APCRMeleeSoldierCharacter
+{
+	GENERATED_BODY()
+	
+};
---
+++ ProjectCardReturn/Source/ProjectCardReturn/Public/Entities/Monsters/Soldier/RangedSoldier/EliteRangedSoldier/PCREliteRangedSoldierCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "ProjectCardReturn.h"
+#include "Entities/Monsters/Soldier/RangedSoldier/PCRRangedSoldierCharacter.h"
+#include "PCREliteRangedSoldierCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTCARDRETURN_API APCREliteRangedSoldierCharacter : public APCRRangedSoldierCharacter
+{
+	GENERATED_BODY()
+	
+};
