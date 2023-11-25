a778b121dcf62b51ffff4b1ebf24f928694b2414
PM 하예찬 <yechandubai@gmail.com>
2023-11-25 23:02:33 +0900

    제네레이터 로직 수정

--- ProjectCardReturn/Source/ProjectCardReturn/Private/Entities/MonsterGenerator/PCRMonsterGenerator.cpp
+++ ProjectCardReturn/Source/ProjectCardReturn/Private/Entities/MonsterGenerator/PCRMonsterGenerator.cpp
@@ -40,38 +40,25 @@ void APCRMonsterGenerator::BeginPlay()
 	UE_LOG(PCRLogMonsterGenerator, Log, TEXT("%s이(가) 성공적으로 생성되었습니다."), *GetName());
 }
 
-void APCRMonsterGenerator::Start(UClass* MonsterClass, float Interval)
+void APCRMonsterGenerator::SpawnMonster(UClass* MonsterClass)
 {
 	if (!ParameterDataAsset->bIsMonsterSpawn)
 	{
 		return;
 	}
-
-	SpawnMonster(MonsterClass);
 	
-	FTimerDelegate SpawnMonsterDelegate;
-	SpawnMonsterDelegate.BindUObject(this, &APCRMonsterGenerator::SpawnMonster, MonsterClass);
-	GetWorldTimerManager().SetTimer(SpawnTimerHandle, SpawnMonsterDelegate, Interval, true);
+	const FVector2D RandomLocation2D = FMath::RandPointInCircle(SpawnRangeRadius);
+	const FVector RandomLocation = FVector(RandomLocation2D.X, RandomLocation2D.Y, 0.0);
 
-	UE_LOG(PCRLogMonsterGenerator, Log, TEXT("%s이(가) 활성화되었습니다."), *GetName());
-}
+	const FVector SpawnLocation = GetActorLocation() + RandomLocation;
 
-void APCRMonsterGenerator::StartOnce(UClass* MonsterClass, int32 SpawnCount)
-{
-	if (!ParameterDataAsset->bIsMonsterSpawn)
+	UE_LOG(PCRLogMonsterGenerator, Log, TEXT("몬스터 스폰 위치: %s"), *SpawnLocation.ToString());
+
+	if (APCRMonsterBaseCharacter* NewMonster = GetWorld()->SpawnActor<APCRMonsterBaseCharacter>(MonsterClass, SpawnLocation, FRotator::ZeroRotator))
 	{
-		return;
+		SpawnMonsters.Add(NewMonster);
+		NewMonster->OnDead.AddUObject(this, &APCRMonsterGenerator::RemoveDeadMonster);
 	}
-
-	for (int i = 0; i < SpawnCount; i++)
-	{
-		SpawnMonster(MonsterClass);
-	}
-}
-
-void APCRMonsterGenerator::Stop()
-{
-	GetWorldTimerManager().ClearTimer(SpawnTimerHandle);
 }
 
 void APCRMonsterGenerator::KillSpawnedMonsters()
@@ -93,22 +80,6 @@ void APCRMonsterGenerator::KillSpawnedMonsters()
 	for (const auto& KillMonster : KillMonsters)
 	{
 		KillMonster->ChangeHP(-99999);
-	}
-}
-
-void APCRMonsterGenerator::SpawnMonster(UClass* MonsterClass)
-{
-	const FVector2D RandomLocation2D = FMath::RandPointInCircle(SpawnRangeRadius);
-	const FVector RandomLocation = FVector(RandomLocation2D.X, RandomLocation2D.Y, 0.0);
-
-	const FVector SpawnLocation = GetActorLocation() + RandomLocation;
-
-	UE_LOG(PCRLogMonsterGenerator, Log, TEXT("몬스터 스폰 위치: %s"), *SpawnLocation.ToString());
-
-	if (APCRMonsterBaseCharacter* NewMonster = GetWorld()->SpawnActor<APCRMonsterBaseCharacter>(MonsterClass, SpawnLocation, FRotator::ZeroRotator))
-	{
-		SpawnMonsters.Add(NewMonster);
-		NewMonster->OnDead.AddUObject(this, &APCRMonsterGenerator::RemoveDeadMonster);
 	}
 }
 
